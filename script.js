28b0a229ce9c3a1b702577f8e5a94f74b8c516c0
vishnuacappella <acappella.studiov@gmail.com>
2022-06-24 06:23:38 +0530

    data type completed

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log(123); //number
+console.log('Vishnu 1234 56 Raj long'); //string
+console.log(true); //boolean
+console.log(1.23); //decimal
+console.log(undefined); //undefined
+console.log(null); //null
+
+var age = 12;
+console.log(age);
+
+var string = "abcd efg 12"
+console.log(string);
+
+
+var boolean = true;
+console.log(boolean);
+
+var decimal = 1.2;
+console.log(decimal);
+
+var time;
+console.log(time);
+
+var time1 = null;
+console.log(time1);
+
+
+console.log("Age: ", age);
+console.log("String: ", string);
+console.log("Boolean: ", boolean);
+console.log("Decimal: ", decimal);
+console.log("Time: ", time);
+
+time = 3;
+console.log("Time: ", time);
+
+
+//! declaration
+// var 123;  //* not valid
+// var 123abc; //* not valid
+// var * abc12; //* not valid
+var _123abc; //* valid
+var _abc; //* valid
+var $abc; //* valid
+var Abc; //* valid
+var abc_def; //* valid
+
+
+var a = 10;
+var a = 5;
+console.log(a);
